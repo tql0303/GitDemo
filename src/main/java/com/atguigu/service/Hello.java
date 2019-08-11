736b23a587cbcdad39c0f3b2525f3cd5249382a0
tql0303 <53959112+tql0303@users.noreply.github.com>
2019-08-11 10:44:15 +0800

    Update Hello.java

--- src/main/java/com/atguigu/service/Hello.java
+++ src/main/java/com/atguigu/service/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.service;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello IDEA Git");
+        System.out.print("抽到女王梅芙了");
     }
 }
