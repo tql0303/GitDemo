2b07149848680f35157a11d87907727e842a46fc
tom_glo <tom@atguigu.com>
2019-08-11 10:58:01 +0800

    切换到hot_fix分支

--- src/main/java/com/atguigu/service/Hello.java
+++ src/main/java/com/atguigu/service/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello IDEA Git");
         System.out.print("抽到女王梅芙了");
+        System.out.println("切换到hot_fix分支");
     }
 }
